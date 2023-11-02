afb7ab1d60ae125d37b14ff1ca5a7602a763f695
nguyenloi <loindp@ipinc.io>
2023-11-02 17:49:55 +0700

    demo - swagger

---
+++ app/controllers/api/v1/pets_controller.rb
@@ -0,0 +1,48 @@
+class Api::V1::PetsController < ApplicationController
+  before_action :set_pet, only: [:show, :update, :destroy]
+
+  #GET /api/v1/pets
+  def index
+    @pets = Pet.all
+    render json: @pets
+  end
+
+  # GET /api/v1/pets/1
+  def show
+    render json: @pet.to_json(only: [:id, :name, :status, :photo_url])
+  end
+
+  # POST /api/v1/pets
+  def create
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      render json: @pet.to_json(only: [:id, :name, :status, :photo_url]), status: :created
+    else
+      render json: @pet.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /api/v1/pets/1
+  def update
+    if @pet.update(pet_params)
+      render json: @pet
+    else
+      render json: @pet.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/v1/pets/1
+  def destroy
+    @pet.destroy
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
+  # Only allow a trusted parameter "white list" through.
+  def pet_params
+    params.fetch(:pet).permit(:name, :status, :photo_url)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  namespace :api do
+    namespace :v1 do
+      resources :pets
+    end
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
